2d46252f40cd5b3cf5a8e0987d78c4f0959fc3fc
Nicolas Costa <nicolascosta.ec@gmail.com>
2025-08-06 08:20:53 -0300

    Importado o Botao_02

--- componentes/src/App.js
+++ componentes/src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Primeiro_Botao from './components/botao/botao'; // Importa função Primeiro_Botao
 import Texto_01 from './components/texto_01/texto'; // Importa função Texto_01
 import Lista_Frutas from './components/lista/lista';
+import Botao_02 from './components/botao2/botao2';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Primeiro_Botao/>
       <Texto_01/>
       <Lista_Frutas/>
+      <Botao_02/>
     </div>
   );
 }
