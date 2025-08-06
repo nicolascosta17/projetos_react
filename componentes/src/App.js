2b0504600c128b75669114f168587815876e5098
Nicolas Costa <nicolascosta.ec@gmail.com>
2025-08-06 08:14:40 -0300

    Importado para meu app

--- componentes/src/App.js
+++ componentes/src/App.js
@@ -2,6 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 import Primeiro_Botao from './components/botao/botao'; // Importa função Primeiro_Botao
 import Texto_01 from './components/texto_01/texto'; // Importa função Texto_01
+import Lista_Frutas from './components/lista/lista';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <h1>Bem-vindo ao meu aplicativo</h1>
       <Primeiro_Botao/>
       <Texto_01/>
+      <Lista_Frutas/>
     </div>
   );
 }
