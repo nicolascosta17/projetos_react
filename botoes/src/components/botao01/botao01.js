b0f6e447de31e04ff888d4a190b5360459d8ed14
Nicolas Costa <nicolascosta.ec@gmail.com>
2025-08-06 10:07:39 -0300

    Criado botão 1 que da mensagem de alerta 'ETEC'

---
+++ botoes/src/components/botao01/botao01.js
@@ -0,0 +1,12 @@
+const Botao_01 = () =>{
+    function click(){
+        alert("ETEC")
+    };
+    return(
+        <button onClick={click}>
+            Botão 1
+        </button>
+    );
+}
+
+export default Botao_01;
