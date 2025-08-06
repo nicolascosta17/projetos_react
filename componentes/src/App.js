2e852737ca9842bec03b794b5b366c9c56d250cb
Nicolas Costa <nicolascosta.ec@gmail.com>
2025-08-06 07:54:59 -0300

    Importado e chamado a tag da função Texto_01

--- componentes/src/App.js
+++ componentes/src/App.js
@@ -1,12 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 import Primeiro_Botao from './components/botao/botao'; // Importa função Primeiro_Botao
+import Texto_01 from './components/texto_01/texto'; // Importa função Texto_01
 
 function App() {
   return (
     <div className="App">
       <h1>Bem-vindo ao meu aplicativo</h1>
       <Primeiro_Botao/>
+      <Texto_01/>
     </div>
   );
 }
