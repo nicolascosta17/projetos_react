1eec00f32eda1f55a37c0ab80975f1565b3eee0c
Nicolas Costa <nicolascosta.ec@gmail.com>
2025-08-06 07:27:04 -0300

    Criado função de Primeiro_Botao

---
+++ componentes/src/components/botao/botao.js
@@ -0,0 +1,7 @@
+function Primeiro_Botao(){ // Função que cria um botão, por enquanto sem ação
+    return(
+        <button>Eu sou um botão</button>
+    );
+}
+
+export default Primeiro_Botao // Exporta função Primeito_Botao
