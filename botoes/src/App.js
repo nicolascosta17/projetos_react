5dcd3806cffdc59c1cb488ae19a9ee069aaef4b6
Nicolas Costa <nicolascosta.ec@gmail.com>
2025-08-06 10:33:43 -0300

    Atualizado o app para puxar os botões

--- botoes/src/App.js
+++ botoes/src/App.js
@@ -2,12 +2,16 @@ import logo from './logo.svg';
 import './App.css';
 import Botao_01 from './components/botao01/botao01';
 import Botao_02 from './components/botao02/botao02';
+import Botao_03 from './components/botao03/botao03';
+import React from './components/botao04/botao04';
 
 function App() {
   return (
     <div className="App">
       <Botao_01/><br></br><br></br>
       <Botao_02/><br></br><br></br>
+      <Botao_03/><br></br><br></br>
+      <React/>
     </div>
   );
 }
