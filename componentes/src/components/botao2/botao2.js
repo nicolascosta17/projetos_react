5b56b6f4b19e45f5e4b29b3a4960e09036d733dc
Nicolas Costa <nicolascosta.ec@gmail.com>
2025-08-06 08:24:15 -0300

    Comentado o aquivo botao2.js

--- componentes/src/components/botao2/botao2.js
+++ componentes/src/components/botao2/botao2.js
@@ -1,11 +1,11 @@
-const Botao_02 = () => { // Arrow Function
-    function handleClick(){
-        alert ("Você clicou no meu botão!");
+const Botao_02 = () => { // Arrow Function (Uma variável que atribui um função)
+    function handleClick(){ // Cria a função handleClick
+        alert ("Você clicou no meu botão!"); // Trás uma mensagem de alerta na tela
     };
-    return(
-        <button onClick={handleClick}>
+    return( // Retorna a função handleClick
+        <button onClick={handleClick}> /* Quando o botão for clicado ele trás a função handleClick 
             Clique aqui
         </button>
     );
 }
-export default Botao_02
+export default Botao_02; // Exporta a função Botao_02
