e87eb99a091d26e9c097ebca71c6cc6ad632d996
Nicolas Costa <nicolascosta.ec@gmail.com>
2025-08-07 08:20:13 -0300

    Criado a função Sobre

---
+++ pages/src/components/sobre/Sobre.js
@@ -0,0 +1,11 @@
+import React from "react";
+function Sobre(){
+    return(
+        <div>
+            <h1>Sobre</h1>
+            <p>Está página é sobre o nosso app</p>
+        </div>
+    );
+}
+
+export default Sobre;
