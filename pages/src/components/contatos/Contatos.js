45cc2d58ac41daef09204805b1bfb3e8149ba7ec
Nicolas Costa <nicolascosta.ec@gmail.com>
2025-08-07 08:19:51 -0300

    Criado a função Contatos

---
+++ pages/src/components/contatos/Contatos.js
@@ -0,0 +1,10 @@
+import React from "react";
+function Contatos(){
+    return(
+        <div>
+            <h1>Contatos</h1>
+            <p>Está página é de contatos</p>
+        </div>
+    );
+}
+export default Contatos;
