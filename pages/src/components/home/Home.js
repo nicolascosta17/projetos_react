5af897a8652a25e802bfe64368e778d87579caa2
Nicolas Costa <nicolascosta.ec@gmail.com>
2025-08-07 08:20:00 -0300

    Criado a função Home

---
+++ pages/src/components/home/Home.js
@@ -0,0 +1,11 @@
+import React from "react";
+function Home(){
+    return(
+        <div>
+            <h1>Bem-vindos à Página Principal</h1>
+            <p>Esta é a página principal do Site.</p>
+        </div>
+    );
+}
+
+export default Home;
