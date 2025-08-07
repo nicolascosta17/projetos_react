2a630e46635a0757e09bf74af848e611f99628f4
Nicolas Costa <nicolascosta.ec@gmail.com>
2025-08-07 10:13:27 -0300

    Ajustes na estilização do App e novo botão Medidas

--- multiplas_paginas/src/App.js
+++ multiplas_paginas/src/App.js
@@ -1,12 +1,42 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
 import Temp from './components/temperaturas/Temperaturas';
+import Home from './components/home/Home';
+import Medidas from './components/medidas/Medidas';
 
 
 function App() {
+  const [page, setPage] = useState('home')
+
+  const rederPageAtual = () => {
+    if (page === 'home'){
+      return <Home/>
+    } else if (page === 'temperatura'){
+      return <Temp/>
+    } else if(page ==='medidas'){
+      return <Medidas/>
+    }
+  }
   return (
     <div className="App">
-      <Temp/>
+      <nav>
+        <ul className='lista'>
+          <li>
+            <label>🏠</label>
+            <button onClick={() => setPage('home')}>Home</button>
+          </li>
+          <li>
+            <label>🌡️</label>
+            <button onClick={() => setPage('temperatura')}>Temperaturas</button>
+          </li>
+          <li>
+            <label>📏</label>
+            <button onClick={() => setPage('medidas')}>Medidas</button>
+          </li>
+        </ul>
+      </nav>
+      {rederPageAtual()}
     </div>
   );
 }
