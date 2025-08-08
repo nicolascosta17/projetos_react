8a3872378cf54f92ba6a404737319b6d51a505a3
Nicolas Costa <nicolascosta.ec@gmail.com>
2025-08-08 09:49:49 -0300

    Criado número

--- form_client/cli.php
+++ form_client/cli.php
@@ -1,2 +1,56 @@
 <?php
 
+
+
+$servername = "localhost";
+$username = "username";
+$password = "password";
+$dbname = "myDB";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+$sql = "INSERT INTO MyGuests (firstname, lastname, email)
+VALUES ('John', 'Doe', 'john@example.com')";
+
+if ($conn->query($sql) === TRUE) {
+  echo "New record created successfully";
+} else {
+  echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+$conn->close();
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cliente</title>
+</head>
+
+<body>
+    <form action="cli.php" method="post">
+        <label for="nome">Nome: </label>
+        <input type="text" name="nome">
+        <br>
+        <label for="cpf">CPF: </label>
+        <input type="text" name="cpf">
+        <br>
+        <label for="rg">RG: </label>
+        <input type="text" name="rg">
+        <br>
+        <label for="email">Email: </label>
+        <input type="text" name="email">
+        <br>
+        <button type="submit">Enviar</button>
+    </form>
+</body>
+
+</html>
